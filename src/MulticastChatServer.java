3ee5663dbc88e1b7f25065cddc4c034affcfd505
lukas.bommelin <lukas.bommelin@elev.ga.ntig.se>
2024-05-13 11:08:11 +0200

    commita ofta

--- src/MulticastChatServer.java
+++ src/MulticastChatServer.java
@@ -1,27 +1,28 @@
 import java.net.*;
+
 public class MulticastChatServer {
     public static void main(String[] args) throws Exception {
 
 
-            //Default port number we are going to use
+        //Default port number we are going to use
         int portnumber = 8080;
-        if (args.length >= 1){
+        if (args.length >= 1) {
             portnumber = Integer.parseInt(args[0]);
         }
 
         //Create a MulticastSocket
         MulticastSocket serverMulticastSocket = new MulticastSocket(portnumber);
-            System.out.println("MulticastSocket is created at port " + portnumber);
+        System.out.println("MulticastSocket is created at port " + portnumber);
 
-            //Determine the IP adress of a host, given the host name
-            InetAddress group = InetAddress.getByName("225.4.5.6");
+        //Determine the IP adress of a host, given the host name
+        InetAddress group = InetAddress.getByName("225.4.5.6");
 
-            //getByName - returns IP adress of given host
+        //getByName - returns IP adress of given host
         serverMulticastSocket.joinGroup(group);
-            System.out.println("joinGroup method is called...");
-            boolean infinite = true;
+        System.out.println("joinGroup method is called...");
+        boolean infinite = true;
 
-            //Continually receives data and prints them
+        //Continually receives data and prints them
         while (infinite) {
             byte buf[] = new byte[1024];
             DatagramPacket data = new DatagramPacket(buf, buf.length);
@@ -31,5 +32,5 @@ public class MulticastChatServer {
         }
         serverMulticastSocket.close();
 
-        }
     }
+}
