e13a7ad3694cb73ffac079f023225b9be7b95bb7
lukas.bommelin <lukas.bommelin@elev.ga.ntig.se>
2024-05-13 10:52:32 +0200

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,13 @@ public class MulticastChatServer {
         throws Exception {
             //Default port number we are going to use
         int portnumber = 8080;
+        if (args.length >= 1){
+            portnumber = Integer.parseInt(args[0]);
+        }
+
+        //Create a MulticastSocket
+        MulticastSocket serverMulticastSocket = new MulticastSocket(portnumber);
+            System.out.println();
 
         }
     }
